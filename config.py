710494d73e9834fef75d60c9ae9671eb186a2f20
GrabCoolGadgets <135804074+GrabCoolGadgets@users.noreply.github.com>
2025-10-24 22:22:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,14 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 class Config:
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
     
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    STORAGE_CHANNEL = int(os.environ.get("STORAGE_CHANNEL"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
+    STORAGE_CHANNEL = int(os.environ.get("STORAGE_CHANNEL", 0))
     
-    BASE_URL = os.environ.get("BASE_URL").rstrip('/')
-    
-    # Yeh nayi line add hui hai
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    BASE_URL = os.environ.get("BASE_URL", "").rstrip('/')
+    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    BLOGGER_PAGE_URL = os.environ.get("BLOGGER_PAGE_URL", "")
