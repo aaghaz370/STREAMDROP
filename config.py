902d11728d84653a32f179e1d915faf99ed732c9
GrabCoolGadgets <135804074+GrabCoolGadgets@users.noreply.github.com>
2025-10-24 23:23:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# config.py (FULL UPDATED CODE)
+# config.py (CLEANED VERSION)
 
 import os
 from dotenv import load_dotenv
@@ -11,14 +11,8 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     OWNER_ID = int(os.environ.get("OWNER_ID", 0))
     
-    # ID ke bajaye ab LINK use karenge
-    LOG_CHANNEL_LINK = os.environ.get("LOG_CHANNEL_LINK", "")
-    STORAGE_CHANNEL_LINK = os.environ.get("STORAGE_CHANNEL_LINK", "")
+    STORAGE_CHANNEL = int(os.environ.get("STORAGE_CHANNEL", 0))
     
     BASE_URL = os.environ.get("BASE_URL", "").rstrip('/')
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
     BLOGGER_PAGE_URL = os.environ.get("BLOGGER_PAGE_URL", "")
-    
-    # Yeh variables hum code ke andar set karenge
-    LOG_CHANNEL = 0
-    STORAGE_CHANNEL = 0
