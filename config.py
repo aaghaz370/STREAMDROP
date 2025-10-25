8199479f842de7c07a618015a2c043e8f33bb1c3
GrabCoolGadgets <135804074+GrabCoolGadgets@users.noreply.github.com>
2025-10-25 09:10:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# config.py (CORRECTED VERSION)
+# config.py (FINAL & CORRECT)
 
 import os
 from dotenv import load_dotenv
@@ -11,7 +11,6 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     OWNER_ID = int(os.environ.get("OWNER_ID", 0))
     
-    # Isko wapas INT hi karna hai. Yeh sahi tareeka hai.
     STORAGE_CHANNEL = int(os.environ.get("STORAGE_CHANNEL", 0))
     
     BASE_URL = os.environ.get("BASE_URL", "").rstrip('/')
